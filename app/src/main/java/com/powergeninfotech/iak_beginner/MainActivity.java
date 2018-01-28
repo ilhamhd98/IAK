fc61ac424db339e7c17aad6878a99e7e26e48583
Alwandy Harold Kalangi <aharoldk@gmail.com>
2018-01-28 16:15:05 +0700

    MainActivity get Intent

--- app/src/main/java/com/powergeninfotech/iak_beginner/MainActivity.java
+++ app/src/main/java/com/powergeninfotech/iak_beginner/MainActivity.java
@@ -3,10 +3,17 @@ package com.powergeninfotech.iak_beginner;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
     private final static String TAG = MainActivity.class.getSimpleName();
+
+    public final static String USERNAME = "username";
+    public final static String PASSWORD = "password";
+
+    private TextView tv_username;
+    private TextView tv_password;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -14,6 +21,16 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         Log.d(TAG, "onCreate: ");
+
+        tv_username = findViewById(R.id.tv_username);
+        tv_password = findViewById(R.id.tv_password);
+
+        String username_intent = getIntent().getStringExtra(USERNAME);
+        String password_intent = getIntent().getStringExtra(PASSWORD);
+
+        tv_username.setText(username_intent);
+        tv_password.setText(password_intent);
+
     }
 
     @Override
