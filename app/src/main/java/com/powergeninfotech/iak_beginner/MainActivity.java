458abcf97de21ebfa0fac230064076df46211c60
Alwandy Harold Kalangi <aharoldk@gmail.com>
2018-01-28 17:04:47 +0700

    Image View with Picasso

--- app/src/main/java/com/powergeninfotech/iak_beginner/MainActivity.java
+++ app/src/main/java/com/powergeninfotech/iak_beginner/MainActivity.java
@@ -3,7 +3,12 @@ package com.powergeninfotech.iak_beginner;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
+import android.widget.ImageView;
 import android.widget.TextView;
+import android.widget.Toast;
+
+import com.squareup.picasso.Callback;
+import com.squareup.picasso.Picasso;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -14,6 +19,9 @@ public class MainActivity extends AppCompatActivity {
 
     private TextView tv_username;
     private TextView tv_password;
+    private ImageView iv_picture;
+
+    private String link_picture = "http://cdn.akc.org/content/hero/puppy-boundaries_header.jpg";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -24,12 +32,27 @@ public class MainActivity extends AppCompatActivity {
 
         tv_username = findViewById(R.id.tv_username);
         tv_password = findViewById(R.id.tv_password);
+        iv_picture = findViewById(R.id.iv_picture);
 
         String username_intent = getIntent().getStringExtra(USERNAME);
         String password_intent = getIntent().getStringExtra(PASSWORD);
 
         tv_username.setText(username_intent);
         tv_password.setText(password_intent);
+
+        Picasso.with(this)
+                .load(link_picture)
+                .into(iv_picture, new Callback() {
+                    @Override
+                    public void onSuccess() {
+                        Toast.makeText(MainActivity.this, "Sukses Terload", Toast.LENGTH_SHORT).show();
+                    }
+
+                    @Override
+                    public void onError() {
+                        Toast.makeText(MainActivity.this, "Gagal Terload", Toast.LENGTH_SHORT).show();
+                    }
+                });
 
     }
 
